afa7cec0af04180859d4748e47c5e69e3ab1e383
0x0u <m0zurillex@gmail.com>
2021-01-25 16:53:42 +0900

    :recycle: テーブル名を複数形にした

--- src/models.py
+++ src/models.py
@@ -3,16 +3,16 @@ from datetime import datetime
 from src import db
 
 
-class User(db.Model):
-    __tablename__ = 'user'
+class Users(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(80), unique=True)
     lang = db.Column(db.String(80), default='ja')
     show_url = db.Column(db.Boolean, default=False)
 
 
-class History(db.Model):
-    __tablename__ = 'history'
+class Histories(db.Model):
+    __tablename__ = 'histories'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(80))
     history = db.Column(db.String(80))
